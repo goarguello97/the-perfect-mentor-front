e8334e14f26727e207c5e6f3fb65f82067329c72
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-12-17 17:15:24 -0300

    feat(ui): add sign up page with responsive layout
    - Add SignUpPage component with mobile and desktop views
    - Add form inputs for username, email, and password
    - Add new SVG assets (email, password, user, mask group, saly-2)
    - Update App routing to include /signup route
    - Refactor MainPage to use dynamic asset paths

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,42 +1,87 @@
-import doodle from "../assets/doodle-5 1.svg";
-import ellipseImage from "../assets/Ellipse 15.svg";
-import salyImage from "../assets/Saly-1.svg";
-import salybug from "../assets/Saly-30.svg";
-import tmpLogo from "../assets/TPM.svg";
+import doodle2 from "@assets/doodle-4 1.svg";
+import doodle from "@assets/doodle-5 1.svg";
+import ellipseImage from "@assets/Ellipse 15.svg";
+import salyImage from "@assets/Saly-1.svg";
+import salybug from "@assets/Saly-30.svg";
+import tmpLogo from "@assets/TPM.svg";
 
-export const MainPage = () => {
+const MainPage = () => {
   return (
-    <div className="bg-[#BFD732] h-screen w-screen flex items-center justify-end flex-col relative">
-      <img
-        src={tmpLogo}
-        alt="The Perfect Mentor"
-        className="absolute left-[30.68px] top-[71px] z-10 w-[200.62]"
-      />
-      <div className="relative w-screen h-screen left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <>
+      <div className="bg-[#BFD732] h-screen w-screen flex items-center justify-end flex-col relative md:hidden">
         <img
-          src={salyImage}
-          alt="Saly"
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          src={tmpLogo}
+          alt="The Perfect Mentor"
+          className="absolute left-[30.68px] top-[71px] z-10 w-[200.62]"
         />
-        <img
-          src={ellipseImage}
-          alt="Ellipse"
-          className="absolute top-[15%] left-[20%]"
-        />
-        <img src={doodle} alt="Doodle" className="absolute top-[15%]" />
-        <img
-          src={salybug}
-          alt="SalyBug"
-          className="absolute bottom-[10%] left-[30%]"
-        />
+        <div className="relative w-screen h-screen left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <img
+            src={salyImage}
+            alt="Saly"
+            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            src={ellipseImage}
+            alt="Ellipse"
+            className="absolute top-[15%] left-[20%]"
+          />
+          <img src={doodle} alt="Doodle" className="absolute top-[15%]" />
+          <img
+            src={salybug}
+            alt="SalyBug"
+            className="absolute bottom-[10%] left-[30%]"
+          />
+        </div>
+
+        <button className="bg-[#444444] rounded-full w-full max-w-[315px] h-[55px] md:h-[60px] text-white font-bold text-[15px] mb-[10px]! z-10 px-4">
+          Sign Up
+        </button>
+        <button className="border-2 border-[#444444] rounded-full w-full max-w-[315px] h-[55px] md:h-[60px] text-[#444444] font-bold text-[15px] mb-[71px]! px-4">
+          Login
+        </button>
       </div>
 
-      <button className="bg-[#444444] rounded-full w-full max-w-[315px] h-[55px] md:h-[60px] text-white font-bold text-[15px] mb-[10px]! z-10 px-4">
-        Sign Up
-      </button>
-      <button className="border-2 border-[#444444] rounded-full w-full max-w-[315px] h-[55px] md:h-[60px] text-[#444444] font-bold text-[15px] mb-[71px]! px-4">
-        Login
-      </button>
-    </div>
+      <div className="hidden md:flex bg-[#BFD732] h-screen w-screen items-center justify-center">
+        <div className="w-[886px] h-[514px] border-2 rounded-[40px] border-[#444444] flex justify-end items-center relative">
+          <img
+            src={salyImage}
+            alt="Saly"
+            className="absolute w-[560px] left-[20%] top-[48%] -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            src={doodle}
+            alt="Doodle"
+            className="absolute left-0 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            src={salybug}
+            alt="SalyBug"
+            className="absolute w-[245px] left-full rotate-64 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            src={doodle2}
+            alt="Doodle2"
+            className="absolute top-[-10%] left-[40%] -translate-x-1/2 -translate-y-1/2"
+          />
+          <div className="w-[50%] h-full flex flex-col justify-between">
+            <img
+              src={tmpLogo}
+              alt="The Perfect Mentor"
+              className="max-w-[248.37px] mt-[110px]!"
+            />
+            <div className="mb-[98px]!">
+              <button className="bg-[#444444] rounded-full w-full max-w-[315px] h-[55px]  text-white font-bold text-[15px] mb-[10px]! z-10 px-4">
+                Sign Up
+              </button>
+              <button className="border-2 border-[#444444] rounded-full w-full max-w-[315px] h-[55px] text-[#444444] font-bold text-[15px] px-4">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
+
+export default MainPage;
