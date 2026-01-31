9a3840679f1252c32d97e0e4918d794ff47e2a31
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2026-01-31 10:02:58 -0300

    feat: implemented interceptor to axiosinstance for send token in requests

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,7 +1,31 @@
 import axios from 'axios';
+import { getAuth } from 'firebase/auth';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      try {
+        const token = await user.getIdToken();
+
+        if (config.headers) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      } catch (error) {
+        console.log('Error obteniendo el Firebase Token', error);
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
